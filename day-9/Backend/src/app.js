fbbe8ae3ac331e84554b1b67e977cf1af780089c
vaidik <vaidikgadhiya49@.com>
2026-02-02 20:10:48 +0530

    feat: initialize backend with Express and MongoDB integration
    - Added package.json for backend dependencies including Express and Mongoose.
    - Created server.js to set up the Express server and connect to the database.
    - Implemented app.js with CRUD operations for notes (create, read, update, delete).
    - Established database connection in database.js using Mongoose.
    - Defined note model in note.model.js for MongoDB schema.

---
+++ day-9/Backend/src/app.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const noteModel=require("./models/note.model")
+
+const app =express();
+app.use(express.json())
+
+app.post("/api/notes",async (req,res)=>{
+    const{ title,description }=req.body;
+
+    const note=await noteModel.create({
+        title,description
+    })
+    res.status(201).json({
+        message:"Note Created Successfully",
+        note
+    })
+})
+
+app.get("/api/notes",async(req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetched successfully",
+        notes
+    })
+})
+
+app.delete("/api/notes/:id",async(req,res)=>{
+    const id =req.params.id;
+
+    await noteModel.findByIdAndDelete(id);
+
+    res.status(200).json({
+        message: "note deleted successfully"
+    })    
+    
+})
+
+app.patch("/api/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const{ description } = req.body;
+
+    await noteModel.findByIdAndUpdate(id,{ description });
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
+})
+
+module.exports=app;
