a69d57c52474984ab15006770d8ad62d5ba457b5
vaidik <vaidikgadhiya49@.com>
2026-02-03 19:27:14 +0530

    Add initial HTML and CSS files for frontend layout
    - Created index.html with basic structure and linked assets
    - Added index-CrV9wFnL.css for styling the application

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const noteModel=require("./models/note.model")
 const cors =require("cors")
+const path = require("path");
 
 const app =express();
 app.use(express.json())
 app.use(cors())
+app.use(express.static("./public"))
 
 app.post("/api/notes",async (req,res)=>{
     const{ title,description }=req.body;
@@ -47,6 +49,12 @@ app.patch("/api/notes/:id",async(req,res)=>{
     res.status(200).json({
         message: "note updated successfully"
     })
+})
+
+console.log(__dirname);
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
 })
 
 module.exports=app;
--- day-9/Frontend/src/App.jsx
+++ day-9/Frontend/src/App.jsx
@@ -1,46 +1,79 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const [notes, setnotes] = useState([]);
 
-  const [notes, setnotes] = useState([
-    {
-    title: "test title 1",
-    description:"Test description"
-  },
-  {
-    title: "test title 2",
-    description:"Test description"
-  },
-  {
-    title: "test title 3",
-    description:"Test description"
-  },
-  {
-    title: "test title 4",
-    description:"Test description"
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setnotes(res.data.notes);
+    });
   }
-])
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-axios.get("http://localhost:3000/api/notes")
-.then(res=>{
-  setnotes(res.data.notes);
-})
+  function submitHandle(e) {
+    e.preventDefault();
+
+    const { title, description } = e.target.elements;
+
+    console.log(title.value, description.value);
+
+    axios
+      .post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+
+        fetchNotes();
+      });
+  }
+
+  function deleteNote(id) {
+    axios.delete("http://localhost:3000/api/notes/"+id)
+    .then(res=>{
+      console.log(res.data);
+      fetchNotes();
+    })
+  }
+
+  function updateNote(id) {
+    axios.patch("http://localhost:3000/api/notes/"+id,{
+      description: "Updated Description"
+    })
+    .then(res=>{
+      console.log(res.data);
+
+      fetchNotes();
+      
+    })
+  }
 
   return (
     <>
+      <form className="note-create-form" onSubmit={submitHandle}>
+        <input name="title" type="text" placeholder="Enter Title" />
+        <input name="description" type="text" placeholder="Enter Description" />
+        <button>Create note</button>
+      </form>
+
       <div className="notes">
-        {
-          notes.map(note=>{
-            return <div className="note">
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              <button onClick={()=> {deleteNote(note._id)}}>delete</button>
+              <button onClick={()=>{updateNote(note._id)}}>Update data</button>
             </div>
-          })
-        }
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
