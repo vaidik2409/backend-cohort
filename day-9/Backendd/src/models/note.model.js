14615b3b160ecbd71ef13bd9286918ab41cebbc7
vaidik <vaidikgadhiya49@.com>
2026-02-04 19:05:44 +0530

    refactor: simplify note model by removing Express app logic and retaining only Mongoose schema

--- day-9/Backendd/src/models/note.model.js
+++ day-9/Backendd/src/models/note.model.js
@@ -1,60 +1,11 @@
-const express=require("express");
-const noteModel=require("./models/note.model")
-const cors =require("cors")
-const path = require("path");
+const mongoose = require("mongoose")
 
-const app =express();
-app.use(express.json())
-app.use(cors())
-app.use(express.static("./public"))
 
-app.post("/api/notes",async (req,res)=>{
-    const{ title,description }=req.body;
-
-    const note=await noteModel.create({
-        title,description
-    })
-    res.status(201).json({
-        message:"Note Created Successfully",
-        note
-    })
+const noteSchema = new mongoose.Schema({
+    title: String,
+    description: String,
 })
 
-app.get("/api/notes",async(req,res)=>{
-    const notes = await noteModel.find()
+const noteModel = mongoose.model("notes", noteSchema)
 
-    res.status(200).json({
-        message:"Notes fetched successfully",
-        notes
-    })
-})
-
-app.delete("/api/notes/:id",async(req,res)=>{
-    const id =req.params.id;
-
-    await noteModel.findByIdAndDelete(id);
-
-    res.status(200).json({
-        message: "note deleted successfully"
-    })    
-    
-})
-
-app.patch("/api/notes/:id",async(req,res)=>{
-    const id = req.params.id
-    const{ description } = req.body;
-
-    await noteModel.findByIdAndUpdate(id,{ description });
-
-    res.status(200).json({
-        message: "note updated successfully"
-    })
-})
-
-console.log(__dirname);
-
-app.use('*name',(req,res)=>{
-    res.sendFile(path.join(__dirname,"..","/public","index.html"))
-})
-
-module.exports=app;
+module.exports = noteModel
